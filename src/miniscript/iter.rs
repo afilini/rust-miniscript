60ab73304662737ffd3df6ff388dec62f3650c6a
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-04-28 15:40:40 +0200

    Miniscript iterators: test cases and bugfixes

--- src/miniscript/iter.rs
+++ src/miniscript/iter.rs
@@ -17,7 +17,7 @@ use super::Error;
 use super::{Miniscript, MiniscriptKey};
 use std::collections::VecDeque;
 use std::ops::Deref;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 /// Iterator-related extensions for [Miniscript]
 impl<PK: MiniscriptKey> Miniscript<PK> {
@@ -127,6 +127,9 @@ impl<PK: MiniscriptKey> Miniscript<PK> {
     /// AST; calls `processor` callback providing each of the found public key or hash and
     /// replaces miniscript item with the new one constructed out of results returned from the
     /// callback. The function returns a new Miniscript AST.
+    ///
+    /// If you need to fail the process due to error, return `None` value from the processor
+    /// and replacement will fail with [Error::PubkeyProcessorFailure] error.
     pub fn replace_pubkeys_and_hashes(
         &self,
         processor: &impl Fn(PubkeyOrHash<PK>) -> Option<PubkeyOrHash<PK>>,
@@ -247,20 +250,18 @@ impl<'a, Pk: MiniscriptKey> Iterator for Iter<'a, Pk> {
     /// To enumerate the branches iterator uses [Miniscript::branches] function.
     fn next(&mut self) -> Option<Self::Item> {
         let mut curr = self.next;
-        match curr {
-            Some(node) => {
-                self.next = node.branches().first().map(|x| *x);
-                self.path.push((node, 1));
-            }
-            None => {
-                while let Some((node, child)) = self.path.pop() {
-                    curr = node.branches().get(child).map(|x| *x);
-                    if curr.is_some() {
-                        self.path.push((node, child + 1));
-                        break;
-                    }
+        if let None = curr {
+            while let Some((node, child)) = self.path.pop() {
+                curr = node.branches().get(child).map(|x| *x);
+                if curr.is_some() {
+                    self.path.push((node, child + 1));
+                    break;
                 }
             }
+        }
+        if let Some(node) = curr {
+            self.next = node.branches().first().map(|x| *x);
+            self.path.push((node, 1));
         }
         curr
     }
@@ -286,8 +287,14 @@ impl<'a, Pk: MiniscriptKey> Iterator for PubkeyIter<'a, Pk> {
     type Item = Pk;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let data = self.node_iter.next()?.get_pubkeys();
-        self.keys_buff = VecDeque::from(data);
+        if self.keys_buff.is_empty() {
+            self.keys_buff = VecDeque::from(loop {
+                let data = self.node_iter.next()?.get_pubkeys();
+                if !data.is_empty() {
+                    break data;
+                }
+            });
+        }
         self.keys_buff.pop_front()
     }
 }
@@ -296,14 +303,14 @@ impl<'a, Pk: MiniscriptKey> Iterator for PubkeyIter<'a, Pk> {
 /// constructs the iterator via [Miniscript::iter_pubkey_hashes] method.
 pub struct PubkeyHashIter<'a, Pk: 'a + MiniscriptKey> {
     node_iter: Iter<'a, Pk>,
-    keys_buff: VecDeque<Pk::Hash>,
+    keyhashes_buff: VecDeque<Pk::Hash>,
 }
 
 impl<'a, Pk: MiniscriptKey> PubkeyHashIter<'a, Pk> {
     fn new(miniscript: &'a Miniscript<Pk>) -> Self {
         PubkeyHashIter {
             node_iter: Iter::new(miniscript),
-            keys_buff: VecDeque::new(),
+            keyhashes_buff: VecDeque::new(),
         }
     }
 }
@@ -312,13 +319,20 @@ impl<'a, Pk: MiniscriptKey> Iterator for PubkeyHashIter<'a, Pk> {
     type Item = Pk::Hash;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let data = self.node_iter.next()?.get_pubkey_hashes();
-        self.keys_buff = VecDeque::from(data);
-        self.keys_buff.pop_front()
+        if self.keyhashes_buff.is_empty() {
+            self.keyhashes_buff = VecDeque::from(loop {
+                let data = self.node_iter.next()?.get_pubkey_hashes();
+                if !data.is_empty() {
+                    break data;
+                }
+            });
+        }
+        self.keyhashes_buff.pop_front()
     }
 }
 
 /// Enum representing either key or a key hash value coming from a miniscript item inside AST
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum PubkeyOrHash<Pk: MiniscriptKey> {
     /// Plain public key
     PlainPubkey(Pk),
@@ -331,14 +345,14 @@ pub enum PubkeyOrHash<Pk: MiniscriptKey> {
 /// [Miniscript::iter_keys_and_hashes] method.
 pub struct PubkeyAndHashIter<'a, Pk: 'a + MiniscriptKey> {
     node_iter: Iter<'a, Pk>,
-    keys_buff: VecDeque<PubkeyOrHash<Pk>>,
+    buff: VecDeque<PubkeyOrHash<Pk>>,
 }
 
 impl<'a, Pk: MiniscriptKey> PubkeyAndHashIter<'a, Pk> {
     fn new(miniscript: &'a Miniscript<Pk>) -> Self {
         PubkeyAndHashIter {
             node_iter: Iter::new(miniscript),
-            keys_buff: VecDeque::new(),
+            buff: VecDeque::new(),
         }
     }
 
@@ -368,10 +382,318 @@ impl<'a, Pk: MiniscriptKey> Iterator for PubkeyAndHashIter<'a, Pk> {
     type Item = PubkeyOrHash<Pk>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.keys_buff.pop_front().or_else(|| {
-            let data = self.node_iter.next()?.get_pubkeys_and_hashes();
-            self.keys_buff = VecDeque::from(data);
-            self.keys_buff.pop_front()
-        })
+        if self.buff.is_empty() {
+            self.buff = VecDeque::from(loop {
+                let data = self.node_iter.next()?.get_pubkeys_and_hashes();
+                if !data.is_empty() {
+                    break data;
+                }
+            });
+        }
+        self.buff.pop_front()
+    }
+}
+
+// Module is public since it export testcase generation which may be used in
+// dependent libraries for their own tasts based on Miniscript AST
+#[cfg(test)]
+pub mod test {
+    use super::{Miniscript, MiniscriptKey};
+    use bitcoin;
+    use bitcoin::hashes::{hash160, ripemd160, sha256, sha256d, Hash};
+    use bitcoin::util::address::AddressType::P2pkh;
+    use bitcoin::{secp256k1, PublicKey};
+    use miniscript::iter::PubkeyOrHash;
+    use std::collections::HashSet;
+    use std::str::FromStr;
+
+    pub type TestData = (
+        Miniscript<bitcoin::PublicKey>,
+        Vec<bitcoin::PublicKey>,
+        Vec<hash160::Hash>,
+        bool, // Indicates that the top-level contains public key or hashes
+    );
+
+    pub fn gen_secp_pubkeys(n: usize) -> Vec<secp256k1::PublicKey> {
+        let mut ret = Vec::with_capacity(n);
+        let secp = secp256k1::Secp256k1::new();
+        let mut sk = [0; 32];
+
+        for i in 1..n + 1 {
+            sk[0] = i as u8;
+            sk[1] = (i >> 8) as u8;
+            sk[2] = (i >> 16) as u8;
+
+            ret.push(secp256k1::PublicKey::from_secret_key(
+                &secp,
+                &secp256k1::SecretKey::from_slice(&sk[..]).unwrap(),
+            ));
+        }
+        ret
+    }
+
+    pub fn gen_bitcoin_pubkeys(n: usize, compressed: bool) -> Vec<bitcoin::PublicKey> {
+        gen_secp_pubkeys(n)
+            .into_iter()
+            .map(|key| bitcoin::PublicKey { key, compressed })
+            .collect()
+    }
+
+    pub fn gen_testcases() -> Vec<TestData> {
+        let k = gen_bitcoin_pubkeys(10, true);
+        let h: Vec<hash160::Hash> = k
+            .iter()
+            .map(|pk| hash160::Hash::hash(&pk.to_bytes()))
+            .collect();
+
+        let preimage = vec![0xab as u8; 32];
+        let sha256_hash = sha256::Hash::hash(&preimage);
+        let sha256d_hash_rev = sha256d::Hash::hash(&preimage);
+        let mut sha256d_hash_bytes = sha256d_hash_rev.clone().into_inner();
+        sha256d_hash_bytes.reverse();
+        let sha256d_hash = sha256d::Hash::from_inner(sha256d_hash_bytes);
+        let hash160_hash = hash160::Hash::hash(&preimage);
+        let ripemd160_hash = ripemd160::Hash::hash(&preimage);
+
+        vec![
+            (ms_str!("after({})", 1000), vec![], vec![], false),
+            (ms_str!("older({})", 1000), vec![], vec![], false),
+            (ms_str!("sha256({})", sha256_hash), vec![], vec![], false),
+            (ms_str!("hash256({})", sha256d_hash), vec![], vec![], false),
+            (ms_str!("hash160({})", hash160_hash), vec![], vec![], false),
+            (
+                ms_str!("ripemd160({})", ripemd160_hash),
+                vec![],
+                vec![],
+                false,
+            ),
+            (ms_str!("c:pk_k({})", k[0]), vec![k[0]], vec![], true),
+            (ms_str!("c:pk_h({})", h[6]), vec![], vec![h[6]], true),
+            (
+                ms_str!("and_v(vc:pk_k({}),c:pk_h({}))", k[0], h[1]),
+                vec![k[0]],
+                vec![h[1]],
+                false,
+            ),
+            (
+                ms_str!("and_b(c:pk_k({}),sjtv:sha256({}))", k[0], sha256_hash),
+                vec![k[0]],
+                vec![],
+                false,
+            ),
+            (
+                ms_str!(
+                    "andor(c:pk_k({}),jtv:sha256({}),c:pk_h({}))",
+                    k[1],
+                    sha256_hash,
+                    h[2]
+                ),
+                vec![k[1]],
+                vec![h[2]],
+                false,
+            ),
+            (
+                ms_str!("multi(3,{},{},{},{},{})", k[9], k[8], k[7], k[0], k[1]),
+                vec![k[9], k[8], k[7], k[0], k[1]],
+                vec![],
+                true,
+            ),
+            (
+                ms_str!(
+                    "thresh(3,c:pk_k({}),sc:pk_k({}),sc:pk_k({}),sc:pk_k({}),sc:pk_k({}))",
+                    k[2],
+                    k[3],
+                    k[4],
+                    k[5],
+                    k[6]
+                ),
+                vec![k[2], k[3], k[4], k[5], k[6]],
+                vec![],
+                false,
+            ),
+            (
+                ms_str!(
+                    "or_d(multi(2,{},{}),and_v(v:multi(2,{},{}),older(10000)))",
+                    k[6],
+                    k[7],
+                    k[8],
+                    k[9]
+                ),
+                vec![k[6], k[7], k[8], k[9]],
+                vec![],
+                false,
+            ),
+            (
+                ms_str!(
+                    "or_d(multi(3,{},{},{},{},{}),\
+                      and_v(v:thresh(2,c:pk_h({}),\
+                      ac:pk_h({}),ac:pk_h({})),older(10000)))",
+                    k[0],
+                    k[2],
+                    k[4],
+                    k[6],
+                    k[9],
+                    h[8],
+                    h[7],
+                    h[0]
+                ),
+                vec![k[0], k[2], k[4], k[6], k[9]],
+                vec![h[8], h[7], h[0]],
+                false,
+            ),
+        ]
+    }
+
+    #[test]
+    fn get_keys() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, test_top_level))| {
+                if !test_top_level {
+                    return;
+                }
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let ms = *ms.branches().first().unwrap_or(&&ms);
+                assert_eq!(ms.get_pubkeys(), k);
+            })
+    }
+
+    #[test]
+    fn get_hashes() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, test_top_level))| {
+                if !test_top_level {
+                    return;
+                }
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let ms = *ms.branches().first().unwrap_or(&&ms);
+                let mut all: Vec<hash160::Hash> = k
+                    .iter()
+                    .map(|p| hash160::Hash::hash(&p.to_bytes()))
+                    .collect();
+                // In our test cases we always have plain keys going first
+                all.extend(h);
+                assert_eq!(ms.get_pubkey_hashes(), all);
+            })
+    }
+
+    #[test]
+    fn get_pubkey_and_hashes() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, test_top_level))| {
+                if !test_top_level {
+                    return;
+                }
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let ms = *ms.branches().first().unwrap_or(&&ms);
+                let r: Vec<PubkeyOrHash<bitcoin::PublicKey>> = if k.is_empty() {
+                    h.into_iter()
+                        .map(|h| PubkeyOrHash::HashedPubkey(h))
+                        .collect()
+                } else {
+                    k.into_iter()
+                        .map(|k| PubkeyOrHash::PlainPubkey(k))
+                        .collect()
+                };
+                assert_eq!(ms.get_pubkeys_and_hashes(), r);
+            })
+    }
+
+    #[test]
+    fn find_keys() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, _, _))| {
+                println!("Test case {}: {:?}", idx + 1, ms);
+                assert_eq!(ms.iter_pubkeys().collect::<Vec<bitcoin::PublicKey>>(), k);
+            })
+    }
+
+    #[test]
+    fn find_hashes() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, _))| {
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let mut all: Vec<hash160::Hash> = k
+                    .iter()
+                    .map(|p| hash160::Hash::hash(&p.to_bytes()))
+                    .collect();
+                // In our test cases we always have plain keys going first
+                all.extend(h);
+                assert_eq!(ms.iter_pubkey_hashes().collect::<Vec<hash160::Hash>>(), all);
+            })
+    }
+
+    #[test]
+    fn find_pubkeys_and_hashes() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, _))| {
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let mut all: Vec<PubkeyOrHash<bitcoin::PublicKey>> = k
+                    .into_iter()
+                    .map(|k| PubkeyOrHash::PlainPubkey(k))
+                    .collect();
+                all.extend(h.into_iter().map(|h| PubkeyOrHash::HashedPubkey(h)));
+                assert_eq!(
+                    ms.iter_pubkeys_and_hashes()
+                        .collect::<Vec<PubkeyOrHash<bitcoin::PublicKey>>>(),
+                    all
+                );
+            })
+    }
+
+    #[test]
+    fn replace_pubkeys_and_hashes() {
+        gen_testcases()
+            .into_iter()
+            .enumerate()
+            .for_each(|(idx, (ms, k, h, _))| {
+                if k.is_empty() && h.is_empty() {
+                    return;
+                }
+                println!("Test case {}: {:?}", idx + 1, ms);
+                let mut all: Vec<PubkeyOrHash<bitcoin::PublicKey>> = k
+                    .into_iter()
+                    .map(|k| PubkeyOrHash::PlainPubkey(k))
+                    .collect();
+                all.extend(h.into_iter().map(|h| PubkeyOrHash::HashedPubkey(h)));
+                // Taking next keys
+                let gen = gen_bitcoin_pubkeys(20, true)[10..20].to_vec();
+                let new: Vec<PubkeyOrHash<bitcoin::PublicKey>> = all
+                    .iter()
+                    .enumerate()
+                    .map(|(index, item)| match item {
+                        PubkeyOrHash::PlainPubkey(_) => PubkeyOrHash::PlainPubkey(gen[index]),
+                        PubkeyOrHash::HashedPubkey(_) => {
+                            PubkeyOrHash::HashedPubkey(hash160::Hash::hash(&gen[index].to_bytes()))
+                        }
+                    })
+                    .collect();
+                let replaced = ms
+                    .replace_pubkeys_and_hashes(&|item| {
+                        let index = all.iter().position(|i| *i == item).unwrap();
+                        match item {
+                            PubkeyOrHash::PlainPubkey(pk) => Some(new[index].clone()),
+                            PubkeyOrHash::HashedPubkey(pkh) => Some(new[index].clone()),
+                        }
+                    })
+                    .unwrap();
+                assert_eq!(
+                    replaced
+                        .iter_pubkeys_and_hashes()
+                        .collect::<Vec<PubkeyOrHash<bitcoin::PublicKey>>>(),
+                    new
+                );
+            })
     }
 }
